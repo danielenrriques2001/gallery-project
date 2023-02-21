27d34692b96b92d549690cc26aff5ae4c8ee759a
daniel <daniel.enrriquesmercedes@gmail.com>
2023-02-21 13:36:18 +0100

    ft: we feched the data from the api and resize functions for the image

--- art-gallery-app/pages/index.js
+++ art-gallery-app/pages/index.js
@@ -1,21 +1,42 @@
+import Error from '../components/Error/Error';
+import Image from "next/image";
 import useSWR from "swr";
 
-export default function HomePage({ random }) {
+export default function HomePage({ random, resize }) {
+
+  
+  
+
+  
+
   const URL = `https://example-apis.vercel.app/api/art`;
 
   const { data, error, isLoading } = useSWR(URL);
 
-  if (data) {
+  if (!data) {return (<Error error = {'the Api is being Fetched'}/>)}
+   
+  if(data) {
+    
     let randomNumber = random(data.length);
-    console.log(randomNumber);
-  }
+
+    let art = data[randomNumber];
+
+    const {artist, name, imageSource, dimensions: {width, height}} = art;
+    
+    return (
+      <div>
+        <h1>{name}</h1>
+        <Image width={resize(width)} height={resize(height)} src={imageSource} alt={`This is ${name} from ${artist}`}></Image>
+      </div>
+    );
+
+   }
+
+    
+    
+  
 
   if (error) return <h1>{error}</h1>;
   if (isLoading) return <h1>{"isLoading..."}</h1>;
 
-  return (
-    <div>
-      <h1>Spotlight</h1>
-    </div>
-  );
 }
