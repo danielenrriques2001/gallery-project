2f51d762fef83532f66e4b11837987c4e6051953
Harry Fosu <harry23adjei@yahoo.com>
2023-02-21 14:50:22 +0100

    updated gallery.js

--- art-gallery-app/pages/gallery.js
+++ art-gallery-app/pages/gallery.js
@@ -1,12 +1,41 @@
-import React from 'react'
+import React from "react";
+import useSWR from "swr";
+import Image from "next/image";
+import Error from "../components/Error/Error";
 
-const gallery = () => {
-  return (
-    <div>
-        <h1>'holsfljslfkd'</h1>
-    </div>
-   
-  )
-}
+const gallery = ({ resize }) => {
+  const URL = `https://example-apis.vercel.app/api/art`;
 
-export default gallery
+  const { data, error, isLoading } = useSWR(URL);
+
+  if (!data) {
+    return <Error error={"the Api is being Fetched"} />;
+  }
+
+  if (data) {
+    return (
+      <div>
+        {data.map((piece) => {
+         return (
+           <>
+             <Image
+               width={resize(piece.dimensions.width)}
+               height={resize(piece.dimensions.height)}
+               src={piece.imageSource}
+               alt={`This is ${piece.name} from ${piece.artist}`}
+             ></Image>
+             <div>
+               <h4>{`${piece.name}`}</h4>
+               <h3>{`${piece.artist}`}</h3>
+             </div>
+           </>
+         );
+
+        })}
+       
+      </div>
+    );
+  }
+};
+
+export default gallery;
